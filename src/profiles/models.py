d7ca41365395d59c24599906b4c8cf4602346889
Neill Henning <neillhenning@gmail.com>
2015-03-03 18:07:58 +0200

    add description to models

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length = 1200)
+    description = models.TextField()
 
 def __unicode__(self):
-        return self.name
+        return self.name # Why the double indentation????!!
