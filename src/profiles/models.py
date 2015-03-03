97c81f428333ea47d8d5fa8997ee2d9177ccdf04
Neill Henning <neillhenning@gmail.com>
2015-03-03 18:10:28 +0200

    handled django error for default

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length = 1200)
-    description = models.TextField()
+    description = models.TextField(default='description default')
 
 def __unicode__(self):
         return self.name # Why the double indentation????!!
