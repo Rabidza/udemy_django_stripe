67e1431c4b0c280edb8f1796851e290cfdab85dc
Neill Henning <neillhenning@gmail.com>
2015-03-09 10:04:56 +0200

    add profiles to admin page

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Profile(models.Model):
+class profile(models.Model):
     name = models.CharField(max_length = 1200)
     description = models.TextField(default='description default')
     ## Remember when you add any more models you have to run
