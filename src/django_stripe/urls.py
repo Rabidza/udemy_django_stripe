73e3bbdda142ca97fc4e56c30d9f8658bec55e0e
Neill Henning <neillhenning@gmail.com>
2015-03-09 11:05:30 +0200

    Changed the way static files are handled if DEBUG == True

--- src/django_stripe/urls.py
+++ src/django_stripe/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'profiles.views.home', name='home'),
@@ -8,3 +11,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
