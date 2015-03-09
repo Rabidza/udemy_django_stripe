b368e6c22115675b4caf06545192953b30048d33
Neill Henning <neillhenning@gmail.com>
2015-03-09 13:30:55 +0200

    Changed the way static files are handled, and added a css file

--- src/django_stripe/urls.py
+++ src/django_stripe/urls.py
@@ -13,5 +13,7 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
+    print 'BEFORE', urlpatterns
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    print 'AFTER', urlpatterns
