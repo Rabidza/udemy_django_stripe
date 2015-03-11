fad28fcfe11abba0ea000a8ff228deed81ae7ae3
Neill Henning <neillhenning@gmail.com>
2015-03-11 15:52:13 +0200

    Change context to use a dictionary instead of locals(), add if statement to stop the submit button from rendering if the form is not displayed.

--- src/contact/views.py
+++ src/contact/views.py
@@ -5,7 +5,10 @@ from django.core.mail import send_mail
 
 # Create your views here.
 def contact(request):
+    title = 'Contact'
     form = contactForm(request.POST or None)
+    context = { 'title': title, 'form':form} # form is needed to keep the form on the html page
+
     if form.is_valid():
         #print request.POST
         # print form.cleaned_data['email']
@@ -16,6 +19,6 @@ def contact(request):
         emailFrom = form.cleaned_data['email']
         emailTo = [settings.EMAIL_HOST_USER]
         send_mail(subject, message, emailFrom, emailTo, fail_silently=True)
-    context = locals()
+    #context = locals() # use dictionary up top instead
     template = 'contact.html'
     return render(request, template, context)
