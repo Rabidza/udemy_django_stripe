9560fec60c5014b1610350fb51e314f4050a5506
Neill Henning <neillhenning@gmail.com>
2015-03-11 15:15:12 +0200

    Add email structure to contact function

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from djanglo.conf import settings
 from .forms import contactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def contact(request):
     form = contactForm(request.POST or None)
     if form.is_valid():
         #print request.POST
-        print form.cleaned_data['email']
+        # print form.cleaned_data['email']
+        comment = form.cleaned_data['comment']
+        name = form.cleaned_data['name']
+        subject = 'Message from django_stripe'
+        message = '%s %s' %(comment, name)
+        emailFrom = form.cleaned_data['email']
+        emailTo = [settings.EMAIL_HOST_USER]
+        send_mail(subject, message, emailFrom, emailTo, fail_silently=True)
     context = locals()
     template = 'contact.html'
     return render(request, template, context)
