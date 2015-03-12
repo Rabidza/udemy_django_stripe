a89fdfea362a91e53e16d8f39dc777eb6de5b350
Neill Henning <neillhenning@gmail.com>
2015-03-12 16:33:00 +0200

    Add profile page to logged in users

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -10,4 +11,13 @@ def home(request):
 def about(request):
     context = {}
     template = 'about.html'
+    return render(request, template, context)
+
+# profile only available to logged in users.
+# @login_required decorator forces user to go to the login page if not logged in
+@login_required
+def profile(request):
+    user = request.user
+    context = {'user': user}
+    template = 'profile.html'
     return render(request, template, context)
