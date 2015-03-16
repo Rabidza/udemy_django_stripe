cd1c01a4a9648277c6b97755c76b09907e54ae93
Neill Henning <neillhenning@gmail.com>
2015-03-16 15:05:26 +0200

    Add userStripe model

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.conf import settings
+from allauth.account.signals import user_logged_in, user_signed_up
 
 # Create your models here.
 class profile(models.Model):
     name = models.CharField(max_length = 1200)
     description = models.TextField(default='description default')
-    
+    # link auth_user to profile
+    # Foreign key
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, blank=True)
     #models below are for demonstration purposes -- asked to remove them by lecturer
     #location = models.CharField(max_length = 1200, default='My location', blank=True)
     #job = models.CharField(max_length = 1200, null=True)
@@ -15,4 +19,14 @@ class profile(models.Model):
 
     def __unicode__(self):
         # unicode is part of the class -- indented the method
-        return self.name 
+        return self.name
+
+class userStripe(model.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    stripe_id = models.CharField(max_length=200, null=True, blank=True)
+
+    def __unicode__(self):
+        if self.stripe_id:
+            return str(self.stripe_id)
+        else:
+            return self.user.username
