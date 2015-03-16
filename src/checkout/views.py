db3335a208effbb02060c0370a7ea9a0c233b779
Neill Henning <neillhenning@gmail.com>
2015-03-16 14:30:16 +0200

    Add stripe secretkey, publishablekey

--- src/checkout/views.py
+++ src/checkout/views.py
@@ -1,9 +1,15 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+secretKey = settings.STRIPE_SECRET_KEY
 # Create your views here.
 @login_required
 def checkout(request):
-    context = {}
+    publishKey = settings.STRIPE_PUBLISHABLE_KEY
+    if request.method == 'POST':
+        token = request.POST['stripeToken']
+        print token
+    context = {'publishKey': publishKey}
     template = 'checkout.html'
     return render(request, template, context)
