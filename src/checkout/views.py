2a93936b1a726ce9d46126e6e572bad6869a0b9b
Neill Henning <neillhenning@gmail.com>
2015-03-16 14:51:48 +0200

    Add charging functionality

--- src/checkout/views.py
+++ src/checkout/views.py
@@ -1,15 +1,27 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import stripe
 
-secretKey = settings.STRIPE_SECRET_KEY
+stripe.api_key = settings.STRIPE_SECRET_KEY
 # Create your views here.
 @login_required
 def checkout(request):
     publishKey = settings.STRIPE_PUBLISHABLE_KEY
     if request.method == 'POST':
         token = request.POST['stripeToken']
-        print token
+
+        # Create the charge on Stripe's servers - this will charge the user's card
+        try:
+            charge = stripe.Charge.create(
+            amount=1000, # amount in cents, again
+            currency="usd",
+            source=token,
+            description="payinguser@example.com"
+            )
+        except stripe.CardError, e:
+            # The card has been declined
+            pass
     context = {'publishKey': publishKey}
     template = 'checkout.html'
     return render(request, template, context)
